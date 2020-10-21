71c052f7a13dc2bbb33ce2370197ff650b68d862
koshel-olga <70837683+koshel-olga@users.noreply.github.com>
2020-10-21 11:57:32 +0400

    feat: add function ReadNumber

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
+function ReadNumber(n, leftDigits, thousands)
+{
+    var ones = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+    var teens = [ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
+    var tens = [ "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
+    var thousandsGroups = [ "", " thousand", " million", " billion" ]
+
+    if (n == 0)
+    {
+        return leftDigits;
+    }
+
+    readInt = leftDigits;
+
+    if (readInt.length > 0)
+    {
+        readInt += " ";
+    }
+    
+    if (n < 10)
+    {
+        readInt += ones[n];
+    }
+    else if (n < 20)
+    {
+        readInt += teens[n - 10];
+    }
+    else if (n < 100)
+    {
+        readInt += ReadNumber(n % 10, tens[parseInt(n / 10) - 2], 0);
+    }
+    else if (n < 1000)
+    {
+        readInt += ReadNumber(n % 100, (ones[parseInt(n / 100)] + " hundred"), 0);
+    }
+    else
+    {
+        readInt += ReadNumber(n % 1000, ReadNumber(parseInt(n / 1000), "", thousands+1), 0);
+        if (n % 1000 == 0)
+        {
+            return readInt;
+        }
+    }
+    return readInt + thousandsGroups[thousands];
 }
+
+
+module.exports = function toReadable (number) {
+    if (number == 0) {
+        return "zero";
+    }
+
+    return ReadNumber(number, "", 0);
+}
+
